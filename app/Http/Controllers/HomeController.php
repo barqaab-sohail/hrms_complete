874ea6cdb6947d770ea7b8153137a00d46dbc265
Sohail Afzal <sohail.afzal@barqaab.com>
2021-01-09 17:17:25 +0500

    09-01-2021

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Hr\HrEmployee;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard.dashboard');
+        
+        $employees = HrEmployee::find(3);
+        return view('dashboard.dashboard', compact('employees'));
     }
     public function testing()
     {
