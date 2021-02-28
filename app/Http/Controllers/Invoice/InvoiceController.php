2c68cbadb4cef2fbab6f99602f937d1d5d12d1c0
Sohail Afzal <sohail.afzal@barqaab.com>
2021-02-28 21:59:59 +0500

    28 feb 2021

--- app/Http/Controllers/Invoice/InvoiceController.php
+++ app/Http/Controllers/Invoice/InvoiceController.php
@@ -15,9 +15,55 @@ use App\Models\Invoice\InvoicePeriod;
 use App\Models\Project\PrDetail;
 use App\Http\Requests\Invoice\InvoiceStore;
 use DB;
+use DataTables;
 
 class InvoiceController extends Controller
 {
+     public function index(){
+
+        $prDetailIds = InvoiceRight::where('hr_employee_id',Auth::user()->hrEmployee->id)->get()->pluck('pr_detail_id')->toArray();
+
+         $projects = PrDetail::wherein('id',$prDetailIds)->get();
+         $invoiceTypes = InvoiceType::all();
+         $costTypes = CostType::all();
+
+        return view ('invoice.index', compact('projects','invoiceTypes','costTypes'));
+    }
+
+    public function show(Request $request, $id){
+        if ($request->ajax()) {
+            $data = Invoice::where('pr_detail_id',$id)->latest()->get();
+
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('invoiceNo', function($row){                
+                      
+                           return $row->invoice_no;
+                           
+                })
+                ->addColumn('status', function($row){                
+                      
+                           return $row->invoice_status_id;
+                           
+                })
+                
+                ->addColumn('Edit', function($row){                
+                  
+                       $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editInvoice">Edit</a>';
+                        return $btn;
+                }) 
+                ->addColumn('Delete', function($row){                
+                  
+                       $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteInvoice">Delete</a>'; 
+                        return $btn;
+                })       
+                ->rawColumns(['Edit','Delete'])
+                ->make(true);
+        }
+
+    }
+
+
     public function create(){
 
     	$prDetailIds = InvoiceRight::where('hr_employee_id',Auth::user()->hrEmployee->id)->get()->pluck('pr_detail_id')->toArray();
@@ -62,6 +108,60 @@ class InvoiceController extends Controller
 
 
     	return response()->json(['status'=> 'OK', 'message' => "Data Successfully Saved"]);
+
+    }
+
+    public function edit($id){
+
+        $invoice = Invoice::with('invoiceCost')->find($id);
+        return response()->json($invoice);
+    }
+
+
+    public function update(InvoiceStore $request, $id){
+
+        $input = $request->all();
+        if($request->filled('from')){
+          $input ['from']= \Carbon\Carbon::parse($request->from)->format('Y-m-d');
+        }
+        if($request->filled('to')){
+        $input ['to']= \Carbon\Carbon::parse($request->to)->format('Y-m-d');
+        }
+
+        if($request->filled('invoice_date')){
+        $input ['invoice_date']= \Carbon\Carbon::parse($request->invoice_date)->format('Y-m-d');
+        }
+
+        DB::transaction(function () use ($input, $id) {  
+
+            Invoice::findOrFail($id)->update($input);
+            $input['invoice_id']= $id;
+
+            // InvoiceCost::create($input);
+
+            // if($input['from']){
+            //     InvoicePeriod::create($input);
+            // }
+
+            // if($input['esc_cost']){
+            //     InvoiceEscalation::create($input);
+            // }
+
+        }); // end transcation
+
+
+        return response()->json(['status'=> 'OK', 'message' => "Data Successfully Saved"]);
+
+    }
+
+
+
+    public function destroy ($id){
+
+        DB::transaction(function () use ($id) {  
+            Invoice::find($id)->delete();              
+        }); // end transcation 
+        return response()->json(['success'=>'data  delete successfully.']);
 
     }
 
