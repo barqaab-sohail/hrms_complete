3b7658b53b7404a4496b2e5f940ba19bda781cee
Sohail Afzal <sohail.afzal@barqaab.com>
2021-07-17 16:35:38 +0500

    17-07-2021

--- routes/web.php
+++ routes/web.php
@@ -157,6 +157,7 @@ Route::resource('/asDocument','AssetDocumentController');
 Route::resource('/asPurchase','AsPurchaseController');
 Route::resource('/asLocation','AsLocationController');
 Route::resource('/asOwnership','AsOwnershipController');
+Route::resource('/asMaintenance','AsMaintenanceController');
 Route::get('/asset/sub_classes/{id?}', 'AssetController@getSubClasses');
 Route::get('/asset/as_code/{id?}', 'AssetController@asCode');
 
