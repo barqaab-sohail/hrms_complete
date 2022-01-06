db888b3b606e16fe0785c53cad0d58865b11e5e0
Sohail Afzal <sohail.afzal@barqaab.com>
2022-01-06 18:13:24 +0500

    06-01-2021

--- app/Models/Leave/LeAccumulative.php
+++ app/Models/Leave/LeAccumulative.php
@@ -11,4 +11,18 @@ class LeAccumulative extends Model implements Auditable
     use \OwenIt\Auditing\Auditable;
     protected $fillable = ['hr_employee_id', 'le_type_id','accumulative_total','date'];
 
+    //get value of hr_status_id and show in string as per statusOptions
+    public function getLeTypeId($attribute)
+    {
+        return $this->statusOptions()[$attribute];
+    }
+    
+    public function statusOptions()
+    {
+        return [
+            1 => 'Casual Leave',
+            2 => 'Annual Leave',
+        ];
+    }
+
 }
