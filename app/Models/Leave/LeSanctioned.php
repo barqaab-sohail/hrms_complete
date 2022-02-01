3a03b5a7012dc67e91855e75db7211ca066a4654
Sohail Afzal <sohail.afzal@barqaab.com>
2022-02-01 21:21:57 +0500

    01-02-2022 2

--- app/Models/Leave/LeSanctioned.php
+++ app/Models/Leave/LeSanctioned.php
@@ -8,5 +8,5 @@ use OwenIt\Auditing\Contracts\Auditable;
 class LeSanctioned extends Model  implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
-    protected $fillable = ['leave_id', 'manager_id','le_status_id','reason']; 
+    protected $fillable = ['leave_id', 'manager_id','le_status_type_id','remarks']; 
 }
