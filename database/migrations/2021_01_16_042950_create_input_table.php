ad92db5db4e6d1ad05c2084e6ad68fe5d9e44188
Sohail Afzal <sohail.afzal@barqaab.com>
2022-02-09 16:23:52 +0500

    09-02-2022

--- database/migrations/2021_01_16_042950_create_input_table.php
+++ database/migrations/2021_01_16_042950_create_input_table.php
@@ -19,12 +19,14 @@ class CreateInputTable extends Migration
             $table->bigInteger('input_project_id')->unsigned();
             $table->bigInteger('hr_employee_id')->unsigned();
             $table->bigInteger('hr_designation_id')->unsigned();
+            $table->bigInteger('pr_detail_id')->unsigned();
             $table->float('input');
             $table->string('remarks')->nullable();
             $table->timestamps();
             $table->foreign('input_project_id')->references('id')->on('input_projects')->onDelete('cascade');
             $table->foreign('hr_employee_id')->references('id')->on('hr_employees');
             $table->foreign('hr_designation_id')->references('id')->on('hr_designations');
+            $table->foreign('pr_detail_id')->references('id')->on('pr_details');
         });
     }
 
