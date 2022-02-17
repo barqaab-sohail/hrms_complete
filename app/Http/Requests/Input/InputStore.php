9a6f8b2c195fb0b9829902463457b52c006aa7fc
Sohail Afzal <sohail.afzal@barqaab.com>
2022-02-17 22:12:32 +0500

    1-02-2022 5

--- app/Http/Requests/Input/InputStore.php
+++ app/Http/Requests/Input/InputStore.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Input;
 
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\Input\Input;
+use App\Models\Input\InputProject;
 
 class InputStore extends FormRequest
 {
@@ -26,10 +27,9 @@ class InputStore extends FormRequest
     {
         
         $employeeId = $this->get('hr_employee_id');
-        $monthId = $this->get('month_id');
         $sumStoredInput = Input::join('input_projects', 'inputs.input_project_id','=', 'input_projects.id')
             ->where('hr_employee_id','=',$employeeId)
-            ->where('input_month_id','=',$monthId)
+            ->where('input_month_id','=',$this->input_month_id)
             ->sum('input');
         
       
@@ -62,12 +62,24 @@ class InputStore extends FormRequest
 
     public function messages()
     {
-       
+        $inputProject = InputProject::where('input_month_id', $this->month_id)->where('pr_detail_id',$this->pr_detail_id)->get();
+        
         return [
-            'hr_employee_id.unique_with' => "This employee input is already entered, $this->input_project_id",
+            'hr_employee_id.unique_with' => "This employee input is already entered",
             'input.max'=>"This employee input exceed from 1"  ,
             'office_department_id.required'=>'Office field is requried'     
             
         ];
+    }
+    protected function getValidatorInstance()
+    {
+        $inputProject = InputProject::where('input_month_id', $this->input_month_id)->where('pr_detail_id',$this->pr_detail_id)->first();
+
+    $data = $this->all();
+    $data['input_project_id'] = $inputProject->id;
+    $this->getInputSource()->replace($data);
+
+    /*modify data before send to validator*/
+    return parent::getValidatorInstance();
     }
 }
