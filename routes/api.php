9341259ee2a3a05429d67c14eb5f4af34c08f619
Sohail Afzal <sohail.afzal@barqaab.com>
2022-03-25 18:14:48 +0500

    25-03-2022

--- routes/api.php
+++ routes/api.php
@@ -21,18 +21,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::Post('/user','Mobile\EmployeeController@user');
 Route::Post('/user/login','Android\Auth\UserController@login');
-Route::get('/asset/classes','Android\Asset\AssetController@classes');
-Route::get('/clients','Android\Asset\AssetController@clients');
-Route::get('/asset/subClasses/{id}','Android\Asset\AssetController@subClasses');
-Route::get('/asset/asset/{id}','Android\Asset\AssetController@show');
+
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
 	
 	Route::get('/user/employee', 'Mobile\EmployeeController@index');
     Route::post('/user/logout', 'Android\Auth\UserController@logout');
 
+    Route::get('/asset/classes','Android\Asset\AssetController@classes');
+	Route::get('/clients','Android\Asset\AssetController@clients');
+	Route::get('/asset/subClasses/{id}','Android\Asset\AssetController@subClasses');
+	Route::get('/asset/asset/{id}','Android\Asset\AssetController@show');
+	Route::post('/asset/store','Android\Asset\AssetController@store');
 
-    
-    Route::post('/asset/store','Android\Asset\AssetController@store');
+   
 
 });
