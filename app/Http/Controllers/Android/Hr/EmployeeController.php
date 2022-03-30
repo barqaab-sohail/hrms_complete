b34e206ae2b21602cc13dae1e363109cf0aff55e
Sohail Afzal <sohail.afzal@barqaab.com>
2022-03-30 17:33:52 +0500

    30-03-2022

--- app/Http/Controllers/Android/Hr/EmployeeController.php
+++ app/Http/Controllers/Android/Hr/EmployeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Android\Hr;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Hr\HrEmployee;
+use App\Models\Hr\HrDocumentation;
 
 class EmployeeController extends Controller
 {
@@ -31,5 +32,52 @@ class EmployeeController extends Controller
 
     	return response()->json($data);
     	
+    }
+
+    public function employees(){
+
+    	// $data = HrEmployee::whereIn('hr_status_id',array(1,5))->get();
+    	// return response()->json($data);
+
+    	$data = HrEmployee::with('employeeDesignation','employeeProject','employeeOffice','employeeAppointment','hrContactMobile')->get();
+
+    	//first sort with respect to Designation
+            $designations = employeeDesignationArray();
+            $data = $data->sort(function ($a, $b) use ($designations) {
+              $pos_a = array_search($a->designation??'', $designations);
+              $pos_b = array_search($b->designation??'', $designations);
+              return $pos_a - $pos_b;
+            });
+       
+     //   // second sort with respect to Hr Status
+            $hrStatuses = array('On Board','Resigned','Terminated','Retired','Long Leave','Manmonth Ended','Death');
+
+            $data = $data->sort(function ($a, $b) use ($hrStatuses) {
+              $pos_a = array_search($a->hr_status_id??'', $hrStatuses);
+              $pos_b = array_search($b->hr_status_id??'', $hrStatuses);
+              return $pos_a - $pos_b;
+            });
+
+         
+         foreach($data as $employee){
+         	$picture = HrDocumentation::where('hr_employee_id',$employee->id)->where('description','Picture')->first();
+         	if($picture){
+         		$picture = asset('storage/'.$picture->path.$picture->file_name);
+         	}
+
+         	$employees[] =  array("id" => $employee->id,
+         						"employee_no" => $employee->employee_no,
+         						"full_name" => $employee->full_name,
+         						"date_of_birth" => \Carbon\Carbon::parse($employee->date_of_birth)->format('M d, Y'),
+         						"date_of_joining"=>\Carbon\Carbon::parse($employee->employeeAppointment->joining_date??'')->format('M d, Y'),
+         						"cnic"=>$employee->cnic,
+         						"designation"=>$employee->designation,
+         						"picture"=>$picture,
+         						"mobile"=>$employee->hrContactMobile->mobile??'',
+         						"status"=>$employee->hr_status_id??'');
+         }
+
+         return response()->json ($employees);
+
     }
 }
