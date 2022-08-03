f62d91d9b893c65cc4b0fcbbd92973b5e3603bf4
Sohail Afzal <sohail.afzal@barqaab.com>
2022-08-03 21:45:04 +0500

    3-08-2022 2

---
+++ app/Http/Requests/Project/PrMonthlyExpenseStore.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests\Project;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PrMonthlyExpenseStore extends FormRequest
+{
+    
+    public function getValidatorInstance()
+    {
+        $this->cleanMonth();
+        return parent::getValidatorInstance();
+    }
+
+    protected function cleanMonth()
+    {
+        if($this->month){
+            $this->merge([
+                'month' => \Carbon\Carbon::parse($this->month)->format('Y-m-d')
+            ]);
+        }
+
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+         $rules = [
+              
+            'month' => 'required|unique_with:pr_monthly_expenses,pr_detail_id,'.$this->expense_id,
+            'salary_expense'=>'required_without:non_salary_expense',
+            'non_salary_expense'=>'required_without:salary_expense',
+    
+        ];
+
+        return $rules;
+    }
+
+    public function messages()
+    {
+        
+        return [
+            'month.unique_with' => 'This Month Expenses already entered',
+            
+        ];
+    }
+}
