09e2e12081c96d085598af1c9ac1b57afffa8deb
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-08-13 15:01:04 -0700

    13-11-2022

--- app/Http/Requests/Project/PrStaffStore.php
+++ app/Http/Requests/Project/PrStaffStore.php
@@ -3,9 +3,49 @@
 namespace App\Http\Requests\Project;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Project\PrStaff;
 
 class PrStaffStore extends FormRequest
 {
+
+
+    public function getValidatorInstance()
+    {
+        $this->cleanDate();
+        return parent::getValidatorInstance();
+    }
+
+    protected function cleanDate()
+    {
+        if ($this->from) {
+            $this->merge([
+                'from' => \Carbon\Carbon::parse($this->from)->format('Y-m-d')
+            ]);
+        }
+        if ($this->to) {
+            $this->merge([
+                'to' => \Carbon\Carbon::parse($this->to)->format('Y-m-d')
+            ]);
+        }
+
+        //check employee enter in already ender dates.
+
+        $data = PrStaff::where('hr_employee_id', $this->hr_employee_id)
+            ->whereDate('from', '<=', $this->from)
+            ->whereDate('to', '>=', $this->from)
+            ->first();
+        //if found than request "from date" set equal to database enter "from date".
+        //so that unique validation fail.
+        if ($data) {
+            $this->merge([
+                'from' => $data->from
+            ]);
+        }
+    }
+
+
+
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -16,6 +56,8 @@ class PrStaffStore extends FormRequest
         return true;
     }
 
+
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -23,16 +65,33 @@ class PrStaffStore extends FormRequest
      */
     public function rules()
     {
-         $rules = [
-              
-            'hr_employee_id' => 'required',
-            'position'=>'required',
-            'from'=>'required',
-            'status'=>'required',
-            'to'=>'required_if:status,Input Ended',
-    
+
+        $rules = [
+
+            'position' => 'required',
+            'from' => 'required|date|unique:pr_staffs,from,' . $this->staff_id,
+            'to' => 'nullable|date|after_or_equal:from|required_if:status,Input Ended|prohibited_if:status,Working',
+            'status' => 'required'
+
         ];
 
+        if ($this->status == 'Working') {
+            $rules += ['hr_employee_id' => 'required|unique_with:pr_staffs,status,' . $this->staff_id];
+        } else {
+            $rules += ['hr_employee_id' => 'required'];
+        }
+
+
         return $rules;
+    }
+
+    public function messages()
+    {
+
+        return [
+            'to.prohibited_if' => "To Date  must be empty, if Staff is Working",
+            'hr_employee_id.unique_with' => 'This Employee is already Entered',
+            'from.unique' => 'These dates Employee is already Entered',
+        ];
     }
 }
