7fd90f60a476d724494f2a8315931490f3d47eea
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-08-17 08:35:36 -0700

    17-08-2022

--- resources/views/hr/verification/index.blade.php
+++ resources/views/hr/verification/index.blade.php
@@ -6,124 +6,38 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
+  <link rel="stylesheet" href="{{asset('verification/style.css')}}" />
 </head>
 
 <body>
-  <style>
-    body,
-    html {
-      height: 100%;
-      width: 100%;
-      margin: 0;
-      padding: 0;
-      background: #fbfbfb !important;
-    }
 
-    .searchbar {
-      margin-top: 50px;
-      height: 60px;
-      background-color: #353b48;
-      border-radius: 30px;
-      padding: 10px;
-    }
-
-    .search_input {
-      color: white;
-      border: 0;
-      outline: 0;
-      background: none;
-      width: 300;
-      caret-color: transparent;
-      line-height: 40px;
-      transition: width 0.4s linear;
-    }
-
-    .searchbar:hover>.search_input {
-      padding: 0 10px;
-      width: 300px;
-      caret-color: red;
-      transition: width 0.4s linear;
-    }
-
-    .searchbar:hover>.search_icon {
-      background: white;
-      color: #e74c3c;
-    }
-
-    .search_icon {
-      height: 40px;
-      width: 40px;
-      float: right;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border-radius: 50%;
-      color: white;
-      text-decoration: none;
-    }
-
-    body{
-    background:#E0E0E0;
-}
-.details {
-            border: 1.5px solid grey;
-            color: #212121;
-            width: 100%;
-            height: auto;
-            box-shadow: 0px 0px 10px #212121;
-        }
-
-        
-        .card {
-            width: fit-content;
-        }
-
-        .card-body {
-            width: fit-content;
-        }
-
-        .btn {
-            border-radius: 0;
-        }
-
-        .img-thumbnail {
-            border: none;
-        }
-
-        .card {
-            box-shadow: 0 20px 40px rgba(0, 0, 0, .2);
-            border-radius: 5px;
-            padding-bottom: 10px;
-        }
-  </style>
   <div class="container">
     <div class="d-flex justify-content-center">
       <div class="searchbar">
-        <input class="search_input" id="search_input" type="text" name="" placeholder="Please Enter CNIC without dash" />
-        <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+        <input class="search_input" id="search_input" type="text" name="" autocomplete="off" placeholder="Please Enter CNIC without dash" />
+        <a href="#" id="search" class="search_icon"><i class="fas fa-search"></i></a>
       </div>
     </div>
   </div>
 
 
   <div class='container-fluid'>
-        <div class="card mx-auto col-md-12 col-10 mt-5">
-            <img class='mx-auto img-thumbnail'
-               
-                width="150" height="auto"/>
-            <div class="card-body text-center mx-auto">
-                <div class='cvp'>
-                    <h5 class="card-title font-weight-bold" id="emp_name"></h5>
-                    <h5 class="card-title font-weight-bold"  id="emp_des"></h5>
-                    <h5 class="card-title font-weight-bold"  id="emp_status"></h5>
-                    
-                </div>
-            </div>
-        </div>
+    <div class="card mx-auto col-md-12 col-10 mt-5">
+      <img class='mx-auto img-thumbnail' width="150" height="auto" />
+      <div class="card-body text-center mx-auto">
+        <div class='cvp'>
+          <h5 class="card-title font-weight-bold" id="emp_name"></h5>
+          <h5 class="card-title font-weight-bold" id="emp_des"></h5>
+          <h5 class="card-title font-weight-bold" id="emp_status"></h5>
 
+        </div>
+      </div>
     </div>
+
+  </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="{{asset('verification\card.js')}}"></script>
 <script type="text/javascript">
   $(document).ready(function() {
     $('.card').hide();
@@ -152,6 +66,19 @@
 
     });
 
+    // Get the input field
+    var input = document.getElementById("search_input");
+    // Execute a function when the user presses a key on the keyboard
+    input.addEventListener("keypress", function(event) {
+      // If the user presses the "Enter" key on the keyboard
+      if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("search").click();
+      }
+    });
+
     $('.search_icon').click(function() {
       var cnic = $("#search_input").val();
       $.ajax({
@@ -168,14 +95,14 @@
             $('.card').show();
             $(".img-thumbnail").show();
             $('#emp_name').text('Name: ' + data.full_name);
-            $('#emp_des').html(' Designation: '+ data.designation);
+            $('#emp_des').html(' Designation: ' + data.designation);
             if (data.hr_status_id == 'Active') {
               $('#emp_status').text('Current Status: Working');
             } else {
               $('#emp_status').text('Current Status: Not Working');
             }
 
-            if(data.picture){
+            if (data.picture) {
               var image = data.picture.path + data.picture.file_name;
               var imageurl = "{{asset('storage/:id')}}".replace(':id', image);
               $(".img-thumbnail").attr('src', imageurl);
@@ -192,7 +119,7 @@
             $(".img-thumbnail").hide();
           }
 
-          
+
 
         },
         error: function(jqXHR, textStatus, errorThrown) {
