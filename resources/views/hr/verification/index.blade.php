b787d07b2f9801b8de7fa20786ef9445be7dd33c
Sohail Afzal <sohail.afzal@barqaab.com>
2022-08-17 21:36:01 +0500

    17-08-2022 2

--- resources/views/hr/verification/index.blade.php
+++ resources/views/hr/verification/index.blade.php
@@ -4,12 +4,22 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <link rel="icon" type="image/png" sizes="16x16" href="{{asset('Massets/images/favicon.ico') }}">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
   <link rel="stylesheet" href="{{asset('verification/style.css')}}" />
 </head>
 
 <body>
+  <div class="container">
+    <div class="d-flex justify-content-center">
+        <img src="{{asset('Massets/images/EmailMono.jpg')}}"/>
+        <br/>
+    </div>
+    <div class="d-flex justify-content-center">
+       <h1>BARQAAB Employee Card Verification</h1>
+    </div>
+  </div>
 
   <div class="container">
     <div class="d-flex justify-content-center">
