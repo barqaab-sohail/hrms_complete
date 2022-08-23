65e52397a391570411a1982594fd9943de9d4df9
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-08-23 16:55:30 -0700

    23-08-2022

---
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project\PrDetail;
+use App\Models\Project\Payment\PaymentReceive;
+use  App\Models\Project\Invoice\Invoice;
+use  App\Models\Project\Invoice\InvoiceCost;
+
+class DashboardController extends Controller
+{
+    public function powerData()
+    {
+        $totalPowerProjectsRunning = PrDetail::where('pr_division_id', 2)->where('pr_status_id', 1)->count();
+        $totalPowerProjectsRunningIds = PrDetail::where('pr_division_id', 2)->where('pr_status_id', 1)->pluck('id')->toArray();
+        $totalNtdcProjectsRunning = PrDetail::where('client_id', 1)->where('pr_division_id', 2)->where('pr_status_id', 1)->count();
+        $totalDiscoProjectsRunning = PrDetail::whereIn('client_id', [6, 7, 8, 9, 10, 11, 12, 13, 14, 15])->where('pr_division_id', 2)->where('pr_status_id', 1)->count();
+        $otherProjectsRunning = PrDetail::where('pr_division_id', 2)->whereNotIn('client_id', [1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])->where('pr_status_id', 1)->count();
+        $totalPowerPaymentReceived = addComma(PaymentReceive::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->sum('amount'));
+        $totalPowerInvoicesId = Invoice::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->pluck('id')->toArray();
+        $totalPowerInvoiceCost = addComma(InvoiceCost::whereIn('invoice_id', $totalPowerInvoicesId)->sum('amount'));
+
+        return response()->json([
+            'total_power_projects_running' => $totalPowerProjectsRunning,
+            'total_ntdc_projects_running' => $totalNtdcProjectsRunning,
+            'total_disco_projects_running' => $totalDiscoProjectsRunning,
+            'other_projects_running' => $otherProjectsRunning,
+            'total_power_payment_received' => $totalPowerPaymentReceived,
+            'total_Power_invoice_cost' => $totalPowerInvoiceCost
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,10 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::post('/user','Mobile\EmployeeController@user');
-Route::post('/user/login','Android\Auth\UserController@login');
+Route::post('/user', 'Mobile\EmployeeController@user');
+Route::post('/user/login', 'Android\Auth\UserController@login');
+
+Route::get('/dashboardData', 'Dashboard\DashboardController@powerData');
 
 
 
@@ -28,19 +30,17 @@ Route::post('/user/login','Android\Auth\UserController@login');
 Route::group(['middleware' => ['auth:sanctum']], function () {
 	//Route::get('/user/employee', 'Mobile\EmployeeController@index');
 	Route::post('/user/logout', 'Android\Auth\UserController@logout');
-   	Route::get('/ageChart','Android\Hr\EmployeeController@ageChart');
-   	Route::get('/hr/employees','Android\Hr\EmployeeController@employees');
-   	Route::get('/hr/employee/documents/{id}','Android\Hr\EmployeeController@documents');
-	
+	Route::get('/ageChart', 'Android\Hr\EmployeeController@ageChart');
+	Route::get('/hr/employees', 'Android\Hr\EmployeeController@employees');
+	Route::get('/hr/employee/documents/{id}', 'Android\Hr\EmployeeController@documents');
 
-//Asset API
-   	Route::get('/asset/employees','Android\Asset\AssetController@employees');
-	Route::get('/asset/offices','Android\Asset\AssetController@offices');
-	Route::get('/asset/classes','Android\Asset\AssetController@classes');
-	Route::get('/clients','Android\Asset\AssetController@clients');
-	Route::get('/asset/subClasses/{id}','Android\Asset\AssetController@subClasses');
-	Route::post('/asset/store','Android\Asset\AssetController@store');
-	Route::get('/asset/asset/{id}','Android\Asset\AssetController@show');
-	
 
+	//Asset API
+	Route::get('/asset/employees', 'Android\Asset\AssetController@employees');
+	Route::get('/asset/offices', 'Android\Asset\AssetController@offices');
+	Route::get('/asset/classes', 'Android\Asset\AssetController@classes');
+	Route::get('/clients', 'Android\Asset\AssetController@clients');
+	Route::get('/asset/subClasses/{id}', 'Android\Asset\AssetController@subClasses');
+	Route::post('/asset/store', 'Android\Asset\AssetController@store');
+	Route::get('/asset/asset/{id}', 'Android\Asset\AssetController@show');
 });
