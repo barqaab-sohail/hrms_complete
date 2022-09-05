2c6a23a7a6534fb30357e3cc2ee96f7c83a47527
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-09-05 17:10:20 +0500

    05-09-2022

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -13,22 +13,30 @@ class DashboardController extends Controller
 {
     public function powerData()
     {
-        $totalPowerProjectsRunning = PrDetail::where('pr_division_id', 2)->where('pr_status_id', 1)->count();
-        $totalPowerProjectsRunningIds = PrDetail::where('pr_division_id', 2)->where('pr_status_id', 1)->pluck('id')->toArray();
+
         $totalNtdcProjectsRunning = PrDetail::where('client_id', 1)->where('pr_division_id', 2)->where('pr_status_id', 1)->count();
         $totalDiscoProjectsRunning = PrDetail::whereIn('client_id', [6, 7, 8, 9, 10, 11, 12, 13, 14, 15])->where('pr_division_id', 2)->where('pr_status_id', 1)->count();
         $otherProjectsRunning = PrDetail::where('pr_division_id', 2)->whereNotIn('client_id', [1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])->where('pr_status_id', 1)->count();
-        $totalPowerPaymentReceived = addComma(PaymentReceive::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->sum('amount'));
-        $totalPowerInvoicesId = Invoice::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->pluck('id')->toArray();
-        $totalPowerInvoiceCost = addComma(InvoiceCost::whereIn('invoice_id', $totalPowerInvoicesId)->sum('amount'));
+
+        $totalPowerProjectsRunning = PrDetail::where('pr_division_id', 2)->where('pr_status_id', 1)->count();
+
+        $totalPowerProjectsRunningIds = PrDetail::where('pr_division_id', 2)->where('pr_status_id', 1)->pluck('id')->toArray();
+        $Received90Days = addComma(PaymentReceive::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->whereBetween('payment_date', [\Carbon\Carbon::now()->subDays(90)->startOfDay(), \Carbon\Carbon::now()->startOfDay()])->sum('amount'));
+        $Received60Days = addComma(PaymentReceive::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->whereBetween('payment_date', [\Carbon\Carbon::now()->subDays(60)->startOfDay(), \Carbon\Carbon::now()->startOfDay()])->sum('amount'));
+
+        $invoice90DaysIds = Invoice::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->whereBetween('invoice_date', [\Carbon\Carbon::now()->subDays(90)->startOfDay(), \Carbon\Carbon::now()->startOfDay()])->pluck('id')->toArray();
+        $Invoice90Days = addComma(InvoiceCost::whereIn('invoice_id', $invoice90DaysIds)->sum('amount'));
+
+        $invoice60DaysIds = Invoice::whereIn('pr_detail_id', $totalPowerProjectsRunningIds)->whereBetween('invoice_date', [\Carbon\Carbon::now()->subDays(60)->startOfDay(), \Carbon\Carbon::now()->startOfDay()])->pluck('id')->toArray();
+        $Invoice60Days = addComma(InvoiceCost::whereIn('invoice_id', $invoice60DaysIds)->sum('amount'));
 
         $porjectData = [
             'total_power_projects_running' => $totalPowerProjectsRunning,
-            'total_ntdc_projects_running' => $totalNtdcProjectsRunning,
-            'total_disco_projects_running' => $totalDiscoProjectsRunning,
             'other_projects_running' => $otherProjectsRunning,
-            'total_power_payment_received' => $totalPowerPaymentReceived,
-            'total_Power_invoice_cost' => $totalPowerInvoiceCost,
+            'received_90_days' => $Received90Days,
+            'invoice_90_days' => $Invoice90Days,
+            'received_60_days' => $Received60Days,
+            'invoice_60_days' => $Invoice60Days,
         ];
 
         $jsonobj = ["{'icon':'<HiOutlineRefresh/>','amount':'39,354','percentage':'-12%', 'title':'Refunds', 'iconColor':'rgb(0, 194, 146)','iconBg':'rgb(235, 250, 242)', 'pcColor': 'red-600'}"];
