864f4cfa8afd50fc26e3f18c17642aed58c39845
Sohail Afzal <sohail.afzal@barqaab.com>
2022-09-17 22:19:02 +0500

    17-09-2022

---
+++ app/Http/Controllers/MIS/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\MIS;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|max:191',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validator_errors' => $validator->messages(),
+
+            ]);
+        } else {
+            $user = User::where('email', $request->email)->first();
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Invalid Credentials',
+                ]);
+            } else {
+                $token = $user->createToken($user->email . '_token')->plainTextToken;
+                return response()->json([
+                    'status' => 200,
+                    'userName' => $user->hrEmployee->full_name,
+                    'token' => $token,
+                    'message' => 'Loogged In Successfully',
+                ]);
+            }
+        }
+    }
+}
