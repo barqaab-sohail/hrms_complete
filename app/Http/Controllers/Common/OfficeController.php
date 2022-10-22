87fa8c6f6dc935210b63179effaf83268d29879e
Sohail Afzal <sohail.afzal@barqaab.com>
2022-10-22 20:35:52 +0500

    22-10-2022

--- app/Http/Controllers/Common/OfficeController.php
+++ app/Http/Controllers/Common/OfficeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Common\OfficeStore;
 use App\Models\Common\Country;
+use App\Models\Hr\HrEmployee;
 use App\Models\Common\Office;
 use App\Models\Common\OfficePhone;
 use DB;
@@ -13,109 +14,129 @@ use DataTables;
 
 class OfficeController extends Controller
 {
-    
-    public function index(Request $request){
-    	if($request->ajax()){
-	    	$data = Office::orderBy('id','desc')->get();
-	    	return DataTables::of($data)	
-	            ->addColumn('phone_no', function($data){
-	        		if($data->officePhones){
-	        			$phone = $data->officePhones->implode('phone_no',' , ');
-	        			return $phone;
-	        		}else{
-	        			return '';
-	        		}
-	                
-	            })
-	            ->addColumn('is_active',function($data){
-	                $status='';
-	                $color = '';
 
-	                if($data->is_active==0){
-	                	$status='Closed';
-	                	$color = 'btn-danger';
-	                }else{
-	                	$status='Working';
-	                	 $color='btn-success';
-	                }
+	public function index(Request $request)
+	{
 
-	            return '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$data->id.'" data-original-title="Edit" class="edit btn '.$color.'  btn-sm editStatus">'.$status.'</a>';  
-	            })
-	            ->addColumn('edit', function($data){
-	        
-	                $button = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$data->id.'" data-original-title="Delete" class="btn btn-success btn-sm editOffice">Edit</a>';
+		if ($request->ajax()) {
+			$data = Office::orderBy('id', 'desc')->get();
+			return DataTables::of($data)
+				->addColumn('hr_employee_id', function ($data) {
+					if ($data->officePhones) {
+						$employees = '';
+						foreach ($data->officePhones as $employee) {
+							$hrEmployee = HrEmployee::find($employee->hr_employee_id);
+							$employees .= $hrEmployee->full_name ?? '';
+							$employees .= '<br>';
+						}
+						return $employees;
+					} else {
+						return '';
+					}
+				})
+				->addColumn('phone_no', function ($data) {
+					if ($data->officePhones) {
+						$phones = '';
+						foreach ($data->officePhones as $phone) {
+							$phones .= $phone->phone_no ?? '';
+							$phones .= '<br>';
+						}
+						return $phones;
+					} else {
+						return '';
+					}
+				})
+				->addColumn('is_active', function ($data) {
+					$status = '';
+					$color = '';
 
-	                return $button;
-	            })
-	            ->addColumn('delete', function($data){
-	                  
-	                $button = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$data->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteOffice">Delete</a>';
-	                return $button;
-	            })
+					if ($data->is_active == 0) {
+						$status = 'Closed';
+						$color = 'btn-danger';
+					} else {
+						$status = 'Working';
+						$color = 'btn-success';
+					}
 
-	            ->rawColumns(['edit','delete','phone_no','is_active'])
-	            ->make(true);
-	    }
-    	return view ('common.office.list');
-   	}
+					return '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $data->id . '" data-original-title="Edit" class="edit btn ' . $color . '  btn-sm editStatus">' . $status . '</a>';
+				})
+				->addColumn('edit', function ($data) {
 
-   	public function create(){
-	    $countries = Country::all(['id','name']);
-		return response()->json(["countries"=>$countries]);
-   	}
+					$button = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $data->id . '" data-original-title="Delete" class="btn btn-success btn-sm editOffice">Edit</a>';
 
-   	public function store (OfficeStore $request){
-		
-		$input = $request->all();
+					return $button;
+				})
+				->addColumn('delete', function ($data) {
 
-        DB::transaction(function () use ($input, $request) {  
+					$button = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $data->id . '" data-original-title="Delete" class="btn btn-danger btn-sm deleteOffice">Delete</a>';
+					return $button;
+				})
 
-           $office = Office::updateOrCreate(['id' => $input['office_id']],$input); 
-
-           if($request->filled("phone_no.0")){
-           		
-           		$officePhoneIds=OfficePhone::where('office_id',$office->id)->get()->pluck('id')->toArray();
-           		$requestOfficePhoneIds = $request->input("office_phone_id");
-           		$officePhones = OfficePhone::where('office_id',$office->id)->get();
-
-           		for ($i=0;$i<count($request->input('phone_no'));$i++){
-	       			$officePhone['office_id'] = $office->id;
-					$officePhone['phone_no'] = $request->input("phone_no.$i");
-					OfficePhone::updateOrCreate(['id' =>$request->input("office_phone_id.$i")],$officePhone);
-				}
-				
-				foreach($officePhones as $officePhone){
-	       			if(!in_array($officePhone->id, $requestOfficePhoneIds)){
-        				OfficePhone::findOrFail($officePhone->id)->delete();
-    				}	
-		       	}	
-           	}else{
-           		$officePhones = OfficePhone::where('office_id',$input['office_id'])->get();
-		       		if($officePhones){
-		       			foreach($officePhones as $officePhone){
-		       				OfficePhone::findOrFail($officePhone->id)->delete();
-		       			}
-		       		}
-           	}
-    	}); // end transcation
-
-		return response()->json(['status'=> 'OK', 'message' => "Submission Successfully Saved"]);
+				->rawColumns(['edit', 'delete', 'phone_no', 'hr_employee_id', 'is_active'])
+				->make(true);
+		}
+		$employees = HrEmployee::all(['id', 'first_name', 'last_name', 'employee_no']);
+		return view('common.office.list', compact('employees'));
 	}
 
-	public function edit($id){
+	public function create()
+	{
+		$countries = Country::all(['id', 'name']);
+		return response()->json(["countries" => $countries]);
+	}
+
+	public function store(OfficeStore $request)
+	{
+
+		$input = $request->all();
+
+		DB::transaction(function () use ($input, $request) {
+
+			$office = Office::updateOrCreate(['id' => $input['office_id']], $input);
+
+			if ($request->filled("phone_no.0")) {
+
+				$officePhoneIds = OfficePhone::where('office_id', $office->id)->get()->pluck('id')->toArray();
+				$requestOfficePhoneIds = $request->input("office_phone_id");
+				$officePhones = OfficePhone::where('office_id', $office->id)->get();
+
+				for ($i = 0; $i < count($request->input('phone_no')); $i++) {
+					$officePhone['office_id'] = $office->id;
+					$officePhone['phone_no'] = $request->input("phone_no.$i");
+					$officePhone['hr_employee_id'] = $request->input("hr_employee_id.$i");
+					OfficePhone::updateOrCreate(['id' => $request->input("office_phone_id.$i")], $officePhone);
+				}
+
+				foreach ($officePhones as $officePhone) {
+					if (!in_array($officePhone->id, $requestOfficePhoneIds)) {
+						OfficePhone::findOrFail($officePhone->id)->delete();
+					}
+				}
+			} else {
+				$officePhones = OfficePhone::where('office_id', $input['office_id'])->get();
+				if ($officePhones) {
+					foreach ($officePhones as $officePhone) {
+						OfficePhone::findOrFail($officePhone->id)->delete();
+					}
+				}
+			}
+		}); // end transcation
+
+		return response()->json(['status' => 'OK', 'message' => "Submission Successfully Saved"]);
+	}
+
+	public function edit($id)
+	{
 		$data = Office::with('officePhones')->find($id);
-    	return response()->json($data);
+		return response()->json($data);
 	}
 
 	public function destroy($id)
-    {
-        DB::transaction(function () use ($id) {  
-            Office::findOrFail($id)->delete();   
-        }); // end transcation
+	{
+		DB::transaction(function () use ($id) {
+			Office::findOrFail($id)->delete();
+		}); // end transcation
 
-        return response()->json(['success'=>'data  delete successfully.']);
-   
-    }
-
-
+		return response()->json(['success' => 'data  delete successfully.']);
+	}
 }
--- app/Models/Common/OfficePhone.php
+++ app/Models/Common/OfficePhone.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class OfficePhone extends Model implements Auditable
-{ 
+{
     use HasFactory;
     use \OwenIt\Auditing\Auditable;
 
-    protected $fillable = ['office_id','phone_no'];
+    protected $fillable = ['office_id', 'phone_no', 'hr_employee_id'];
 }
