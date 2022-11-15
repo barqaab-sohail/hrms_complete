ef05e142846b7d657d354fc519106df28f105685
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-11-15 14:46:57 +0500

    15-11-2022

--- app/Http/Controllers/Hr/HrAlertController.php
+++ app/Http/Controllers/Hr/HrAlertController.php
@@ -8,121 +8,120 @@ use App\Models\Hr\HrEmployee;
 
 class HrAlertController extends Controller
 {
-    
-    public function alertList(){
-    	$nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
-    	$totalCnicExpire = HrEmployee::where('hr_status_id',1)->where('cnic_expiry','<',$nextTenDays)->count();
 
-    	$appointmentExpiryTotal = appointmentExpiryTotal();
+    public function alertList()
+    {
+        $nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
+        $totalCnicExpire = HrEmployee::where('hr_status_id', 1)->where('cnic_expiry', '<', $nextTenDays)->count();
+
+        $appointmentExpiryTotal = appointmentExpiryTotal();
         $drivingLicenceExpiryTotal = drivingLicenceExpiryTotal();
         $pecCardExpiryTotal = pecCardExpiryTotal();
-    	
-    	return view('hr.alert.list',compact('totalCnicExpire','appointmentExpiryTotal','drivingLicenceExpiryTotal','pecCardExpiryTotal'));
-	}
 
-	public function cnicExpiryDetail(){
-		$nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
-		$employees = HrEmployee::where('hr_status_id',1)->where('cnic_expiry','<',$nextTenDays)->with('employeeProject','employeeOffice')->get();
+        return view('hr.alert.list', compact('totalCnicExpire', 'appointmentExpiryTotal', 'drivingLicenceExpiryTotal', 'pecCardExpiryTotal'));
+    }
 
-      	foreach ($employees as $employee){
-    		$data [] = array(
-				"employee_name" => employeeFullName($employee->id),
-                "employee_project"=>$employee->employeeProject->last()->name??'',
-                "employee_office"=>$employee->employeeOffice->last()->name??'',
-				"cnic_expiry_date" =>\Carbon\Carbon::parse( $employee->cnic_expiry)->format('M d, Y'),
-                "mobile"=>$employee->hrContactMobile->mobile??'',
-			);  			
-    	}
+    public function cnicExpiryDetail()
+    {
+        $nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
+        $employees = HrEmployee::where('hr_status_id', 1)->where('cnic_expiry', '<', $nextTenDays)->with('employeeProject', 'employeeOffice')->get();
 
-    	usort($data, function($a, $b) {
-    		return strtotime($a['cnic_expiry_date']) - strtotime($b['cnic_expiry_date']);
-		});
+        foreach ($employees as $employee) {
+            $data[] = array(
+                "employee_name" => employeeFullName($employee->id),
+                "employee_project" => $employee->employeeProject->last()->name ?? '',
+                "employee_office" => $employee->employeeOffice->last()->name ?? '',
+                "cnic_expiry_date" => \Carbon\Carbon::parse($employee->cnic_expiry)->format('M d, Y'),
+                "mobile" => $employee->hrContactMobile->mobile ?? '',
+            );
+        }
 
-    	return response()->json(['status'=> 'Ok', 'full_name'=>'CNIC Expiry Detail', 'cnicExpiry'=>$data]);
-	}
+        usort($data, function ($a, $b) {
+            return strtotime($a['cnic_expiry_date']) - strtotime($b['cnic_expiry_date']);
+        });
 
-	public function appointmentExpiry(){
-		$nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
-		$employees = HrEmployee::where('hr_status_id',1)->with('employeeAppointment','employeeOffice','employeeProject')->get();
+        return response()->json(['status' => 'Ok', 'full_name' => 'CNIC Expiry Detail', 'cnicExpiry' => $data]);
+    }
 
-		foreach ($employees as $key => $employee) {                   
-            if($employee->employeeAppointment->expiry_date??''!=''){
-                if($employee->employeeAppointment->expiry_date<$nextTenDays){
-            		$data [] = array(
-					"employee_name" => employeeFullName($employee->id),
-                    "employee_project"=>$employee->employeeProject->last()->name??'',
-                    "employee_office"=>$employee->employeeOffice->last()->name??'',
-					"appointment_expiry_date" =>  \Carbon\Carbon::parse( $employee->employeeAppointment->expiry_date)->format('M d, Y'),
-                    "mobile"=>$employee->hrContactMobile->mobile??'',
-					);  	
-                    
+    public function appointmentExpiry()
+    {
+        $nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
+        $employees = HrEmployee::where('hr_status_id', 1)->with('employeeAppointment', 'employeeOffice', 'employeeProject')->get();
+
+        foreach ($employees as $key => $employee) {
+            if ($employee->employeeAppointment->expiry_date ?? '' != '') {
+                if ($employee->employeeAppointment->expiry_date < $nextTenDays) {
+                    $data[] = array(
+                        "employee_name" => employeeFullName($employee->id),
+                        "employee_project" => $employee->employeeProject->last()->name ?? '',
+                        "employee_office" => $employee->employeeOffice->last()->name ?? '',
+                        "appointment_expiry_date" =>  \Carbon\Carbon::parse($employee->employeeAppointment->expiry_date)->format('M d, Y'),
+                        "mobile" => $employee->hrContactMobile->mobile ?? '',
+                    );
                 }
             }
-            
         }
 
-    	usort($data, function($a, $b) {
-    		return strtotime($a['appointment_expiry_date']) - strtotime($b['appointment_expiry_date']);
-		});
+        usort($data, function ($a, $b) {
+            return strtotime($a['appointment_expiry_date']) - strtotime($b['appointment_expiry_date']);
+        });
 
-    	return response()->json(['status'=> 'Ok', 'full_name'=>'Appointment Expiry Detail', 'appointmentExpiry'=>$data]);
-	}
+        return response()->json(['status' => 'Ok', 'full_name' => 'Appointment Expiry Detail', 'appointmentExpiry' => $data]);
+    }
 
-    public function drivingLicenceExpiry(){
+    public function drivingLicenceExpiry()
+    {
         $nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
-        $employees = HrEmployee::where('hr_status_id',1)->with('employeeDesignation','employeeProject','employeeOffice','hrDriving')->get();
+        $employees = HrEmployee::where('hr_status_id', 1)->with('employeeDesignation', 'employeeProject', 'employeeOffice', 'hrDriving')->get();
 
-        foreach ($employees as $key => $employee) {                   
-            if($employee->employeeDesignation->last()->name??''=='Driver'){
-                if($employee->hrDriving->licence_expiry??''!=''){
-                    if($employee->hrDriving->licence_expiry<$nextTenDays){
-                        $data [] = array(
-                        "employee_name" => employeeFullName($employee->id),
-                        "employee_project"=>$employee->employeeProject->last()->name??'',
-                        "employee_office"=>$employee->employeeOffice->last()->name??'',
-                        "licence_expiry_date" => \Carbon\Carbon::parse($employee->hrDriving->licence_expiry)->format('M d, Y'),
-                        "mobile"=>$employee->hrContactMobile->mobile??'',
-                        );      
-                        
+        foreach ($employees as $key => $employee) {
+            if ($employee->employeeDesignation->last()->name ?? '' == 'Driver') {
+                if ($employee->hrDriving->licence_expiry ?? '' != '') {
+                    if ($employee->hrDriving->licence_expiry < $nextTenDays) {
+                        $data[] = array(
+                            "employee_name" => employeeFullName($employee->id),
+                            "employee_project" => $employee->employeeProject->last()->name ?? '',
+                            "employee_office" => $employee->employeeOffice->last()->name ?? '',
+                            "licence_expiry_date" => \Carbon\Carbon::parse($employee->hrDriving->licence_expiry)->format('M d, Y'),
+                            "mobile" => $employee->hrContactMobile->mobile ?? '',
+                        );
                     }
                 }
-            }   
+            }
         }
 
-        usort($data, function($a, $b) {
+        usort($data, function ($a, $b) {
             return strtotime($a['licence_expiry_date']) - strtotime($b['licence_expiry_date']);
         });
 
-        return response()->json(['status'=> 'Ok', 'full_name'=>'Driver Licence Expiry Detail', 'drivingLicenceExpiryTotal'=>$data]);
+        return response()->json(['status' => 'Ok', 'full_name' => 'Driver Licence Expiry Detail', 'drivingLicenceExpiryTotal' => $data]);
     }
 
-    public function pecCardExpiry(){
+    public function pecCardExpiry()
+    {
         $nextTenDays = \Carbon\Carbon::now()->addDays(10)->format('Y-m-d');
-        $employees = HrEmployee::where('hr_status_id',1)->with('hrMembership','employeeOffice','employeeProject')->get();
+        $employees = HrEmployee::where('hr_status_id', 1)->with('hrMembership', 'employeeOffice', 'employeeProject')->get();
 
-        foreach ($employees as $key => $employee) {                   
-            
-            if($employee->hrMembership->expiry??''!=''){
-                if($employee->hrMembership->expiry<$nextTenDays){
-                    $data [] = array(
-                    "employee_name" => employeeFullName($employee->id),
-                    "employee_project"=>$employee->employeeProject->last()->name??'',
-                    "employee_office"=>$employee->employeeOffice->last()->name??'',
-                    "pec_expiry_date" => \Carbon\Carbon::parse($employee->hrMembership->expiry)->format('M d, Y'),
-                    "mobile"=>$employee->hrContactMobile->mobile??'',
-                    );      
-                    
+        foreach ($employees as $key => $employee) {
+
+            if ($employee->hrMembership->expiry ?? '' != '') {
+                if ($employee->hrMembership->expiry < $nextTenDays) {
+                    $data[] = array(
+                        "employee_name" => employeeFullName($employee->id),
+                        "employee_project" => $employee->employeeProject->last()->name ?? '',
+                        "employee_office" => $employee->employeeOffice->last()->name ?? '',
+                        "pec_expiry_date" => \Carbon\Carbon::parse($employee->hrMembership->expiry)->format('M d, Y'),
+                        "mobile" => $employee->hrContactMobile->mobile ?? '',
+                        "pec" => $employee->hrMembership->membership_no ?? '',
+                    );
                 }
             }
-             
         }
 
-        usort($data, function($a, $b) {
+        usort($data, function ($a, $b) {
             return strtotime($a['pec_expiry_date']) - strtotime($b['pec_expiry_date']);
         });
 
-        return response()->json(['status'=> 'Ok', 'full_name'=>'PEC Card Expiry Detail', 'pecCardExpiry'=>$data]);
+        return response()->json(['status' => 'Ok', 'full_name' => 'PEC Card Expiry Detail', 'pecCardExpiry' => $data]);
     }
-
-
 }
--- resources/views/hr/alert/alertDetailModal.blade.php
+++ resources/views/hr/alert/alertDetailModal.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
-<div class="modal fade" id="alertDetailModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
-  <div class="modal-dialog  modal-lg" >
+<div class="modal fade" id="alertDetailModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog  modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h3 class="modal-title" id="alert_name">Detail of Alert</h3>
@@ -9,184 +9,191 @@
         </button>
       </div>
       <div class="modal-body">
-        
-          <div class="table-responsive">
 
-                <table id="alertDetail" class="table table-bordered table-striped" width="100%" cellspacing="0">
-                    <thead >
-                    
-                      <tr >
-                          <th style="font-weight:bold">Employee Name</th>
-                          <th id="project" style="font-weight:bold">Project/Office</th>
-                          <th style="font-weight:bold">Expiry Date</th>
-                          <th style="font-weight:bold">Mobile</th>
-                      </tr>
-                    </thead>
-                    <tbody id="tbody">
-                       
-                   
-                    </tbody>
-                </table>
- 
+        <div class="table-responsive">
+
+          <table id="alertDetail" class="table table-bordered table-striped" width="100%" cellspacing="0">
+            <thead>
+
+              <tr>
+                <th style="font-weight:bold">Employee Name</th>
+                <th id="project" style="font-weight:bold">Project/Office</th>
+                <th style="font-weight:bold">Expiry Date</th>
+                <th style="font-weight:bold">Mobile</th>
+                <th style="font-weight:bold">PEC No</th>
+              </tr>
+            </thead>
+            <tbody id="tbody">
+
+
+            </tbody>
+          </table>
+
+        </div>
+
       </div>
-      
     </div>
   </div>
-</div>
+  <style>
+    .modal-dialog {
+      max-width: 80%;
+      display: flex;
+    }
+  </style>
 
 
-
-<script>
-
-//$(document).on('click', 'button[id^=detail]',function(){ 
+  <script>
+    //$(document).on('click', 'button[id^=detail]',function(){ 
 
 
-$('#cnicExpiryDetail').click(function(e){ 
-  e.preventDefault(); 
-  
-  var url = $(this).attr('href');
-     $.ajax({
-     url:url,
-     dataType:"json",
-     success:function(data){
-        $('#alert_name').text(data.full_name);
-        $('#alertDetail > tbody').empty();
-        // $('#project').remove();      
-        $.each(data.cnicExpiry, function() {
-          var office='';
-          if (this.employee_project == 'overhead'){
-            office = this.employee_office;
-          }else{
-             office = this.employee_project;
-          }
-          $('#alertDetail > tbody').append(
-              '<tr><td>'
-              + this.employee_name
-              + '</td><td>'
-              + office
-              + '</td><td>'
-              + this.cnic_expiry_date
-              + '</td><td>'
-              + this.mobile
-              + '</td></tr>'
-          );
-        });
-     }
-    });
-    $('#alertDetailModel').modal('show');
-});
-
-$('#appointmentExpiryDetail').click(function(e){ 
-  e.preventDefault();
-
-  var url = $(this).attr('href');
-     $.ajax({
-     url:url,
-     dataType:"json",
-    
-     success:function(data){
-
-        $('#alert_name').text(data.full_name);
-        $('#alertDetail > tbody').empty();
-        $.each(data.appointmentExpiry, function() {
-          var office='';
-          if (this.employee_project == 'overhead'){
-            office = this.employee_office;
-          }else{
-             office = this.employee_project;
-          }
-          $('#alertDetail > tbody').append(
-              '<tr><td>'
-              + this.employee_name
-              + '</td><td>'
-              + office
-              + '</td><td>'
-              + this.appointment_expiry_date
-              + '</td><td>'
-              + this.mobile
-              + '</td></tr>'
-          );
-        });
-        $('#alertDetailModel').modal('show');
-     }     
-    });
-});
-
-$('#drivingLicenceExpiryTotal').click(function(e){ 
-    e.preventDefault();
-  var url = $(this).attr('href');
-     $.ajax({
-     url:url,
-     dataType:"json",
-     
-     success:function(data){
-
-        $('#alert_name').text(data.full_name);
-        $('#alertDetail > tbody').empty();
-        $.each(data.drivingLicenceExpiryTotal, function() {
-          var office='';
-          if (this.employee_project == 'overhead'){
-            office = this.employee_office;
-          }else{
-             office = this.employee_project;
-          }
-         
-          $('#alertDetail > tbody').append(
-              '<tr><td>'
-              + this.employee_name
-              + '</td><td>'
-              + office
-              + '</td><td>'
-              + this.licence_expiry_date
-              + '</td><td>'
-              + this.mobile
-              + '</td></tr>'
-          );
-        });
+    $('#cnicExpiryDetail').click(function(e) {
+      e.preventDefault();
+      var url = $(this).attr('href');
+      $.ajax({
+        url: url,
+        dataType: "json",
+        success: function(data) {
+          $('#alert_name').text(data.full_name);
+          $('#alertDetail > tbody').empty();
+          $("#alertDetail th:last-child, #alertDetail td:last-child").hide();
+          // $('#project').remove();      
+          $.each(data.cnicExpiry, function() {
+            var office = '';
+            if (this.employee_project == 'overhead') {
+              office = this.employee_office;
+            } else {
+              office = this.employee_project;
+            }
+            $('#alertDetail > tbody').append(
+              '<tr><td>' +
+              this.employee_name +
+              '</td><td>' +
+              office +
+              '</td><td>' +
+              this.cnic_expiry_date +
+              '</td><td>' +
+              this.mobile +
+              '</td></tr>'
+            );
+          });
+        }
+      });
       $('#alertDetailModel').modal('show');
-     }
     });
- 
-});
 
-$('#pecCardExpiry').click(function(e){ 
-    e.preventDefault();
-    var url = $(this).attr('href');
-     $.ajax({
-     url:url,
-     dataType:"json",
-     
-     success:function(data){
+    $('#appointmentExpiryDetail').click(function(e) {
+      e.preventDefault();
 
-        $('#alert_name').text(data.full_name);
-        $('#alertDetail > tbody').empty(); 
-        $.each(data.pecCardExpiry, function() {
-          var office='';
-          if (this.employee_project == 'overhead'){
-            office = this.employee_office;
-          }else{
-             office = this.employee_project;
-          }
+      var url = $(this).attr('href');
+      $.ajax({
+        url: url,
+        dataType: "json",
 
-          $('#alertDetail > tbody').append(
-              '<tr><td>'
-              + this.employee_name
-              + '</td><td>'
-              + office
-              + '</td><td>'
-              + this.pec_expiry_date
-              + '</td><td>'
-              + this.mobile
-              + '</td></tr>'
-          );
-        });
-      $('#alertDetailModel').modal('show');
-     }
+        success: function(data) {
+
+          $('#alert_name').text(data.full_name);
+          $('#alertDetail > tbody').empty();
+          $("#alertDetail th:last-child, #alertDetail td:last-child").hide();
+          $.each(data.appointmentExpiry, function() {
+            var office = '';
+            if (this.employee_project == 'overhead') {
+              office = this.employee_office;
+            } else {
+              office = this.employee_project;
+            }
+            $('#alertDetail > tbody').append(
+              '<tr><td>' +
+              this.employee_name +
+              '</td><td>' +
+              office +
+              '</td><td>' +
+              this.appointment_expiry_date +
+              '</td><td>' +
+              this.mobile +
+              '</td></tr>'
+            );
+          });
+          $('#alertDetailModel').modal('show');
+        }
+      });
     });
-});
 
+    $('#drivingLicenceExpiryTotal').click(function(e) {
+      e.preventDefault();
+      var url = $(this).attr('href');
+      $.ajax({
+        url: url,
+        dataType: "json",
 
+        success: function(data) {
 
-</script>
+          $('#alert_name').text(data.full_name);
+          $('#alertDetail > tbody').empty();
+          $("#alertDetail th:last-child, #alertDetail td:last-child").hide();
+          $.each(data.drivingLicenceExpiryTotal, function() {
+            var office = '';
+            if (this.employee_project == 'overhead') {
+              office = this.employee_office;
+            } else {
+              office = this.employee_project;
+            }
 
-<!--end Model--> 
+            $('#alertDetail > tbody').append(
+              '<tr><td>' +
+              this.employee_name +
+              '</td><td>' +
+              office +
+              '</td><td>' +
+              this.licence_expiry_date +
+              '</td><td>' +
+              this.mobile +
+              '</td></tr>'
+            );
+          });
+          $('#alertDetailModel').modal('show');
+        }
+      });
+
+    });
+
+    $('#pecCardExpiry').click(function(e) {
+      e.preventDefault();
+      var url = $(this).attr('href');
+      $.ajax({
+        url: url,
+        dataType: "json",
+
+        success: function(data) {
+
+          $('#alert_name').text(data.full_name);
+          $('#alertDetail > tbody').empty();
+          $("#alertDetail th:last-child, #alertDetail td:last-child").show();
+          $.each(data.pecCardExpiry, function() {
+            var office = '';
+            if (this.employee_project == 'overhead') {
+              office = this.employee_office;
+            } else {
+              office = this.employee_project;
+            }
+
+            $('#alertDetail > tbody').append(
+              '<tr><td>' +
+              this.employee_name +
+              '</td><td>' +
+              office +
+              '</td><td>' +
+              this.pec_expiry_date +
+              '</td><td>' +
+              this.mobile +
+              '</td><td>' +
+              this.pec +
+              '</td></tr>'
+            );
+          });
+          $('#alertDetailModel').modal('show');
+        }
+      });
+    });
+  </script>
+
+  <!--end Model-->
