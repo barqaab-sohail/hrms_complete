d0ad200799c772535c6869bf3513bd4bb7375ee9
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-12-12 09:24:29 +0500

    12-12-2022

--- resources/views/layouts/master/master.blade.php
+++ resources/views/layouts/master/master.blade.php
@@ -15,13 +15,13 @@
     <meta name="user_id" content="{{Auth::check() ? Auth::user()->id :''}}">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" rel="stylesheet" />
-    
+
     <!-- Jquery UI Datepicker "Redmond theme" CSS -->
     <link href="{{asset('Massets/js/jquery-ui/jquery-ui.min.css') }}" rel="stylesheet">
 
     <!-- Bootstrap Core CSS -->
     <link href="{{asset('Massets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-   
+
     <!-- JS Validation  CSS -->
     <link href="{{asset('Massets/css/js-validation/js-validation.min.css')}}" rel="stylesheet" type="text/css" />
 
@@ -34,9 +34,9 @@
     <!-- Custom CSS -->
 
     <link href="{{asset('Massets/css/hrstyle.css') }}" rel="stylesheet">
-     <link href="{{asset('Massets/select2/select2.min.css') }}" rel="stylesheet">
+    <link href="{{asset('Massets/select2/select2.min.css') }}" rel="stylesheet">
     <!-- You can change the theme colors from here -->
-    <link href="{{asset('Massets/css/colors/green.css') }}" id="theme" rel="stylesheet">
+    <link href="{{asset('Massets/css/colors/red.css') }}" id="theme" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -53,68 +53,68 @@
     <!-- start - This is for datatabe Fixed Columns only -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css" />
-     <link rel="stylesheet" href="https://cdn.datatables.net/fixedcolumns/3.2.6/css/fixedColumns.bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/fixedcolumns/3.2.6/css/fixedColumns.bootstrap.min.css" />
 
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.css" />
 
-     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">     
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
     <!-- end - This is for datatabe Fixed Columns only -->
-    
-    
+
+
     @include('layouts.master.adminScripts')
-    
+
 </head>
 <style>
-thead th {
-  font-weight: bold;
-}
+    thead th {
+        font-weight: bold;
+    }
 </style>
 
 <body class="fix-header fix-sidebar card-no-border">
-<!-- ============================================================== -->
-<!-- Preloader - style you can find in spinners.css -->
-<!-- ============================================================== -->
-<div class="preloader">
-    <svg class="circular" viewBox ="25 25 50 50">
-        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"></circle> </svg>
-</div>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"></circle>
+        </svg>
+    </div>
 
 
-<!-- ============================================================== -->
-<!-- Main wrapper - style you can find in pages.scss -->
-<!-- ============================================================== -->
-<div id="main-wrapper">
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
 
-    @include('layouts.master.adminHeader')
-    @include('layouts.master.adminNav')
-    <div class="page-wrapper">
-        <!-- ============================================================== -->
-        <!-- Container fluid  -->
-        <!-- ============================================================== -->
-        <div class="container-fluid">
+        @include('layouts.master.adminHeader')
+        @include('layouts.master.adminNav')
+        <div class="page-wrapper">
             <!-- ============================================================== -->
-            <!-- Bread crumb and right sidebar toggle -->
+            <!-- Container fluid  -->
             <!-- ============================================================== -->
-            <div class="row page-titles" >
-                <div class="col-md-12 col-8 align-self-center" >
-                    <h3 class="text-themecolor"> @yield('h3')</h3>
-                    @yield('Heading')
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Bread crumb and right sidebar toggle -->
+                <!-- ============================================================== -->
+                <div class="row page-titles">
+                    <div class="col-md-12 col-8 align-self-center">
+                        <h3 class="text-themecolor"> @yield('h3')</h3>
+                        @yield('Heading')
 
+                    </div>
                 </div>
-            </div>
 
-                @include('layouts.master.message') 
+                @include('layouts.master.message')
 
                 @yield('content')
+            </div>
         </div>
     </div>
-</div>
 
-@yield('footer')
+    @yield('footer')
     <div style="text-align:center">
         <p>HRMS 5.10 - Developed by: BARQAAB IT</p>
     </div>
 </body>
 
 </html>
-
