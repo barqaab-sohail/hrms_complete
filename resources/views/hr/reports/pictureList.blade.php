ed13ab6537356b9f5857be08f4987892782eb702
Sohail Afzal <sohail.afzal@barqaab.com>
2022-12-22 21:10:18 +0500

    22-12-2022

--- resources/views/hr/reports/pictureList.blade.php
+++ resources/views/hr/reports/pictureList.blade.php
@@ -1,42 +1,39 @@
 @extends('layouts.master.master')
 @section('title', 'BARQAAB HR')
 @section('Heading')
-<h3 class="text-themecolor">List of HR Reports</h3>
+<h3 class="text-themecolor">List of Employees</h3>
 @stop
 @section('content')
 <div class="card">
     <div class="card-body">
-        <h4 class="card-title">List of HR Reports</h4>
+        <h4 class="card-title">List of Employees Picture</h4>
 
         <div class="table-responsive m-t-40">
             <table id="myTable" class="table table-bordered table-striped" style="width:100%">
                 <thead>
                     <tr>
+                        <th>Employee No</th>
+                        <th>Employee Name</th>
+                        <th>Designation</th>
+                        <th>Size of Picture</th>
+                        <th class="text-center" style="width:5%">Edit</th>
 
-                        <th>Name of Report</th>
-                        <th class="text-center" style="width:5%">Show</th>
                     </tr>
                 </thead>
-
                 <tbody>
+                    @foreach($employees as $employee)
+                    <tr>
+                        <td>{{$employee->employee_no??''}}</td>
+                        <td>{{$employee->first_name}} {{$employee->last_name}}</td>
+                        <td>{{$employee->employeeCurrentDesignation->name??''}}</td>
+                        <td>{{round(($employee->picture->size??0 / 1000),2)}}</td>
+                        <td class="text-center">
+                            <a class="btn btn-success btn-sm" href="{{route('employee.edit',$employee->id)}}" title="Edit"><i class="fas fa-pencil-alt text-white "></i></a>
+                        </td>
 
-                    <tr>
-                        <td><a href="{{route('hrReports.cnicExpiryList')}}" style="color:black">CNIC Expiry List</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href="{{route('hrReports.missingDocumentList')}}" style="color:black">Missing Document List</a></td>
 
                     </tr>
-
-                    <tr>
-                        <td><a href="{{route('hrReports.searchEmployee')}}" style="color:black">Search Employee</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href="{{route('hrReports.report_1')}}" style="color:black">Report_1 (Father Name, CNIC, Degree, Degree Year, DOJ, PEC, Employee No., Contact No.)</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href="{{route('hrReports.pictureList')}}" style="color:black">Employee Pictures</a></td>
-                    </tr>
+                    @endforeach
 
                 </tbody>
             </table>
@@ -52,29 +49,30 @@
 
         $('#myTable').DataTable({
             stateSave: false,
+            "ordering": false,
 
             dom: 'Blfrtip',
             buttons: [{
                     extend: 'copyHtml5',
                     exportOptions: {
-                        columns: [0, 1]
+                        columns: [0, 1, 2, 3]
                     }
                 },
                 {
                     extend: 'excelHtml5',
                     exportOptions: {
-                        columns: [0, 1]
+                        columns: [0, 1, 2, 3]
                     }
                 },
                 {
                     extend: 'pdfHtml5',
                     exportOptions: {
-                        columns: [0, 1]
+                        columns: [0, 1, 2, 3]
                     }
                 }, {
                     extend: 'csvHtml5',
                     exportOptions: {
-                        columns: [0, 1]
+                        columns: [0, 1, 2, 3]
                     }
                 },
             ],
