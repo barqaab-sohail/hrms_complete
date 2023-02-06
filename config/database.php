fb4ad2d4032d2515fe09679dcf43133d80a158f6
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2023-02-06 14:12:55 +0500

    06-02-2022

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'dump' => [
-               // 'dump_binary_path' => 'C:/xampp/mysql/bin/', // only the path, so without `mysqldump` or `pg_dump`
+                // 'dump_binary_path' => 'C:/xampp/mysql/bin/', // only the path, so without `mysqldump` or `pg_dump`
                 'use_single_transaction',
                 'timeout' => 60 * 5, // 5 minute timeout
                 //'exclude_tables' => ['table1', 'table2'],
@@ -131,7 +131,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
--- resources/views/hr/verification/show.blade.php
+++ resources/views/hr/verification/show.blade.php
@@ -40,7 +40,7 @@
                     <h5 class="card-title font-weight-bold" id="emp_des">Emergency No: {{$data->hrEmergency->mobile??''}}</h5>
                     <h5 class="card-title font-weight-bold" id="emp_status">{{$data->hr_status_id == 'Active'?'Current Status: Working':'Current Status: Not Working'}}</h5>
                     <br>
-                    {!! '<img src="data:image/png;base64,'. DNS2D::getBarcodePNG(url('cardVerificationResult').'/'.$data->employee_no,'QRCODE',5,5). '" width="300" height="auto" alt="barcode" />' !!}
+                    <!-- {!! '<img src="data:image/png;base64,'. DNS2D::getBarcodePNG(url('cardVerificationResult').'/'.$data->employee_no,'QRCODE',5,5). '" width="300" height="auto" alt="barcode" />' !!} -->
 
                 </div>
             </div>
