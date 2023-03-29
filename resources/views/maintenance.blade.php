c2e5fc0c7b943c6a6e919a5fd3bc41e062f80945
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2023-03-29 09:49:02 +0500

    29-03-2024

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.auth.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>{{config('app.name')}} is Under Maintenance</>
+        </div>
+    </div>
+</div>
+
+@endsection
