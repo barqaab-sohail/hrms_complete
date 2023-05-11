8ec8162686ce4d07bfdc17c1154825c1904d0ac8
Sohail Afzal <sohail.afzal@barqaab.com>
2023-05-11 22:12:35 +0500

    11-05-2023

--- app/Helper/functions.php
+++ app/Helper/functions.php
@@ -9,17 +9,17 @@ use App\Models\Common\Office;
 
 function rightsName($id)
 {
-	if ($id == 1) {
-		return 'No Access';
-	} else if ($id == 2) {
-		return 'View Record';
-	} else if ($id == 3) {
-		return 'Edit Record';
-	} else if ($id == 4) {
-		return 'Delete Record';
-	} else {
-		return '';
-	}
+    if ($id == 1) {
+        return 'No Access';
+    } else if ($id == 2) {
+        return 'View Record';
+    } else if ($id == 3) {
+        return 'Edit Record';
+    } else if ($id == 4) {
+        return 'Delete Record';
+    } else {
+        return '';
+    }
 }
 
 
@@ -27,69 +27,69 @@ function rightsName($id)
 
 function projectPaymentRight($project)
 {
-	if (Auth::user()->hasRole('Super Admin')) {
-		return true;
-	}
+    if (Auth::user()->hasRole('Super Admin')) {
+        return true;
+    }
 
-	$projectPaymentRight = PrRight::where('hr_employee_id', Auth::user()->hrEmployee->id)->where('pr_detail_id', $project)->first();
-	if ($projectPaymentRight) {
-		if ($projectPaymentRight->invoice == 1) {
-			return false;
-		} else {
-			return $projectPaymentRight->payment;
-		}
-	} else {
-		return false;
-	}
+    $projectPaymentRight = PrRight::where('hr_employee_id', Auth::user()->hrEmployee->id)->where('pr_detail_id', $project)->first();
+    if ($projectPaymentRight) {
+        if ($projectPaymentRight->invoice == 1) {
+            return false;
+        } else {
+            return $projectPaymentRight->payment;
+        }
+    } else {
+        return false;
+    }
 }
 
 function projectProgressRight($project)
 {
-	if (Auth::user()->hasRole('Super Admin')) {
-		return true;
-	}
+    if (Auth::user()->hasRole('Super Admin')) {
+        return true;
+    }
 
-	$projectProgressRight = PrRight::where('hr_employee_id', Auth::user()->hrEmployee->id)->where('pr_detail_id', $project)->first();
+    $projectProgressRight = PrRight::where('hr_employee_id', Auth::user()->hrEmployee->id)->where('pr_detail_id', $project)->first();
 
-	if ($projectProgressRight) {
-		if ($projectProgressRight->progress == 1) {
-			return false;
-		} else {
-			return $projectProgressRight->progress;
-		}
-	} else {
-		return false;
-	}
+    if ($projectProgressRight) {
+        if ($projectProgressRight->progress == 1) {
+            return false;
+        } else {
+            return $projectProgressRight->progress;
+        }
+    } else {
+        return false;
+    }
 }
 
 function addComma($id)
 {
 
-	if ($id) {
+    if ($id) {
 
-		return number_format($id);
-	} else {
-		return '';
-	}
+        return number_format($id);
+    } else {
+        return '';
+    }
 }
 
 function removeComma($id)
 {
-	$value = intval(str_replace(',', '', $id));
-	return $value;
+    $value = intval(str_replace(',', '', $id));
+    return $value;
 }
 
 
 function officeName($officeId)
 {
-	if ($officeId) {
+    if ($officeId) {
 
-		$office = Office::find($officeId);
+        $office = Office::find($officeId);
 
-		return $office->name;
-	} else {
-		return '';
-	}
+        return $office->address;
+    } else {
+        return '';
+    }
 }
 
 
@@ -97,25 +97,25 @@ function officeName($officeId)
 function cvSpecilizationName($id)
 {
 
-	$cvSpecialization = CvSpecialization::find($id);
+    $cvSpecialization = CvSpecialization::find($id);
 
-	return $cvSpecialization->name;
+    return $cvSpecialization->name;
 }
 
 function fullName($id)
 {
 
-	$cvDetail = CvDetail::find($id);
+    $cvDetail = CvDetail::find($id);
 
-	return $cvDetail->full_name;
+    return $cvDetail->full_name;
 }
 
 
 function age($dob)
 {
-	//return $dob->diffInYears(\Carbon::now());
-	$years = \Carbon\Carbon::parse($dob)->age;
-	return $years;
+    //return $dob->diffInYears(\Carbon::now());
+    $years = \Carbon\Carbon::parse($dob)->age;
+    return $years;
 }
 
 
@@ -123,9 +123,9 @@ function age($dob)
 function countPdfPages($path)
 {
 
-	$pdftext = json_decode(file_get_contents($path));
+    $pdftext = json_decode(file_get_contents($path));
 
-	$num = preg_match_all("/\/Page\W/", $pdftext, $dummy);
+    $num = preg_match_all("/\/Page\W/", $pdftext, $dummy);
 
-	return $num;
+    return $num;
 }
