cc52bf73d51f6adfa625d8dd942d3d0f2f4a3878
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2023-05-26 14:45:30 +0500

    26-05-2023

--- app/Http/Controllers/MIS/Project/ProjectLedgerActivityController.php
+++ app/Http/Controllers/MIS/Project/ProjectLedgerActivityController.php
@@ -15,36 +15,32 @@ class ProjectLedgerActivityController extends Controller
     {
 
         $checkUpdate = "NO";
-
         $lastUpdate = LedgerActivity::where('pr_detail_id', $projectId)->orderBy('updated_at', 'desc')->first();
-        $difference = 11;
+        $difference = 29;
         if ($lastUpdate) {
-            $lastUpdate =  \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $lastUpdate->updated_at);
+            $lastUpdate =  \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $lastUpdate->updated_at)->format('d-m-Y');
             $today = \Carbon\Carbon::now();
-            $difference = $today->diffInDays($lastUpdate);
-        }
-        $statusCode = '400';
-        if ($difference > 10) {
-            $ledger = new Ledger();
-            $resonseData = $ledger->importLedgerActivity($projectId);
-            $checkUpdate = "Yes";
-            $statusCode =  response()->json($resonseData->status());
+            $difference = $today->diffInMinutes($lastUpdate);
         }
 
-        if ($statusCode == '200' || $lastUpdate) {
+        $statusCode = 400;
+        if ($difference > 30 || !$lastUpdate) {
+            $ledger = new Ledger();
+            $ledgerResponse = $ledger->importLedgerActivity($projectId);
+            $checkUpdate = "Yes";
+            $statusCode = $ledgerResponse->status();
+        }
+
+        $project = PrDetail::find($projectId);
+        $projectCost = isset($project->prCost) ? (int)$project->prCost->total_cost : 0;
+
+        if ($statusCode == 200) {
             $totalDebit = LedgerActivity::where('pr_detail_id', $projectId)->sum('debit');
             $totalCredit = LedgerActivity::where('pr_detail_id', $projectId)->sum('credit');
             $balance = $totalDebit - $totalCredit;
-            $project = PrDetail::find($projectId);
-            if ($project->prCost) {
-                $projectCost = (int)$project->prCost->total_cost ?? '';
-            } else {
-                $projectCost = '';
-            }
-
-            return response()->json(['total_debit' => $totalDebit, 'total_credit' => $totalCredit, 'balance' => $balance, 'projectCost' => $projectCost, 'update' => $checkUpdate]);
+            return response()->json(['total_debit' => $totalDebit, 'total_credit' => $totalCredit, 'balance' => $balance, 'last_update' => $lastUpdate, 'projectCost' => $projectCost, 'update' => $checkUpdate]);
         } else {
-            return response()->json(['total_debit' => 0, 'total_credit' => 0, 'balance' => 0, 'projectCost' => 0, 'update' => $checkUpdate]);
+            return response()->json(['total_debit' => 0, 'total_credit' => 0, 'balance' => 0, 'projectCost' => $projectCost, 'last_update' => $lastUpdate, 'update' => $checkUpdate]);
         }
     }
 }
