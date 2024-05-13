dcad84c038371a71bf1418592acba350f54d12e4
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2024-05-13 16:09:32 +0500

    13-05-2024

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,11 +49,11 @@ return [
             'permissions' => [
                 'file' => [
                     'public' => 0644,
-                    'private' => 0644,
+                    'private' => 0600,
                 ],
                 'dir' => [
                     'public' => 0755,
-                    'private' => 0755,
+                    'private' => 0700,
                 ],
             ],
         ],
