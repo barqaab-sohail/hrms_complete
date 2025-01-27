7f515b3659fc0d8fa6955f412c99ec53edc25900
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2025-01-27 09:18:06 +0500

    27-02-2025

--- resources/views/layouts/master/adminNav.blade.php
+++ resources/views/layouts/master/adminNav.blade.php
@@ -174,7 +174,7 @@
                 <!-- Photocopy -->
                 @canany(['photocopy record'])
                 <li class="{{Request::is('photocopy*')?'active':''}}"> <a class="has-arrow waves-effect waves-dark"
-                        href="#" aria-expanded="false"><i class="mdi mdi-bank"></i><span
+                        href="#" aria-expanded="false"><i class="mdi mdi-printer"></i><span
                             class="hide-menu">Photocopy</span></a>
                     <ul aria-expanded="false" class="collapse">
                         @can('Super Admin')        
