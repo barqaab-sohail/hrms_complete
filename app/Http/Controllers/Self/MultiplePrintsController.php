5ee047fdbb52d94fd4d23f11e44e566299ce8636
Sohail Afzal <sohail.afzal@barqaab.com>
2026-02-25 15:16:10 +0500

    25-02-2025

--- app/Http/Controllers/Self/MultiplePrintsController.php
+++ app/Http/Controllers/Self/MultiplePrintsController.php
@@ -62,14 +62,14 @@ class MultiplePrintsController extends Controller
             $size = $fpdi->getTemplateSize($template);
             $fpdi->AddPage($size['orientation'], array($size['width'], $size['height']));
             $fpdi->useTemplate($template);
-            $fpdi->Image($image2, 11, 20, 90, 0, 'png');
-            $fpdi->Image($image2, 105, 20, 90, 0, 'png');
-            $fpdi->Image($image2, 11, 85, 90, 0, 'png');
-            $fpdi->Image($image2, 105, 85, 90, 0, 'png');
-            $fpdi->Image($image2, 11, 150, 90, 0, 'png');
-            $fpdi->Image($image2, 105, 150, 90, 0, 'png');
-            $fpdi->Image($image2, 11, 215, 90, 0, 'png');
-            $fpdi->Image($image2, 105, 215, 90, 0, 'png');
+            $fpdi->Image($image2, 10, 20, 90, 0, 'png');
+            $fpdi->Image($image2, 102, 20, 90, 0, 'png');
+            $fpdi->Image($image2, 10, 85, 90, 0, 'png');
+            $fpdi->Image($image2, 102, 85, 90, 0, 'png');
+            $fpdi->Image($image2, 10, 150, 90, 0, 'png');
+            $fpdi->Image($image2, 102, 150, 90, 0, 'png');
+            $fpdi->Image($image2, 10, 215, 90, 0, 'png');
+            $fpdi->Image($image2, 102, 215, 90, 0, 'png');
         }
         $fpdi->Output($outputFilePath, 'F');
     }
